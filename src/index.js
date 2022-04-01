1ee15c88e76a8975c64a20bfbbc10716c2c35031
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-04-01 20:56:45 +0100

    remove header option

--- src/index.js
+++ src/index.js
@@ -22,10 +22,6 @@ of this function will now be as expected.
     )
     .option('-f, --files <path>', 'Path of csv files')
     .option(
-        '-h, --headers-row <header rows>',
-        'Total number of headers row. Default is 1'
-    )
-    .option(
         '-o, --output <output file name>',
         'Output file name. Default is output.csv'
     )
