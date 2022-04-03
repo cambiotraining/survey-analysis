811d9f8a6a88d3451b9a47e0cd4d015724a4d1fd
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-04-03 17:27:35 +0100

    wip: extract

--- src/commands/extract.js
+++ src/commands/extract.js
@@ -1,3 +1,28 @@
-const extract = (args = {}) => {}
+const chalk = require('chalk')
+
+const processInputFilePattern = require('../common/process-input-file-pattern')
+const logger = require('../utils/logger')
+const { catUsingFileList } = require('../utils/concatnate')
+
+const extract = (extractType, other) => {
+    if (extractType !== 'contact') {
+        logger(chalk.bold.red('Error: This tool can only extract contact.'))
+        return
+    }
+
+    const { files: pattern } = other
+
+    const files = processInputFilePattern(pattern)
+
+    if (!files) {
+        return
+    }
+
+    const df = catUsingFileList(files)
+
+    df.columns(['Contact info: Name']).print()
+    // df.print()
+    // df.print()
+}
 
 module.exports = extract
