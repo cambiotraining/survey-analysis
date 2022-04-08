338a089c75d92f59edc4eb5c4bfd5414b4b6ca25
Ankit Kumar <ankits.kvmdam@hotmail.com>
2022-04-08 16:46:12 +0100

    add an option to add meta data during concatenating

--- src/utils/concatnate.js
+++ src/utils/concatnate.js
@@ -1,33 +1,56 @@
 const Papa = require('papaparse')
 const fs = require('fs-extra')
 const dfd = require('danfojs-node')
+const path = require('path')
+
+/**
+ * @typedef {object} CatOptions
+ * @property {boolean} addMetaData add meta data
+ */
 
 /**
  * To build danfo DataFrame from csv file
  *
  * @param {string} filePath File path of csv file
+ * @param {CatOptions} option Cat options
  * @returns {dfd.DataFrame} danfo DataFrame
  */
-const buildDataFrameFromCSVFilePath = (filePath) => {
+const buildDataFrameFromCSVFilePath = (filePath, option) => {
     const rawData = fs.readFileSync(filePath, { encoding: 'utf8' })
     const parseData = Papa.parse(rawData, { header: true })
-    const parseDataList = parseData.data.map((data) => Object.values(data))
+    const rows = parseData.data.map((data) => Object.values(data))
 
-    return new dfd.DataFrame(parseDataList, {
-        columns: Object.keys(parseData.data[0]),
+    const columns = Object.keys(parseData.data[0])
+
+    if (option.addMetaData) {
+        console.log('Adding meta data')
+        columns.push('Meta Data')
+
+        const meta = JSON.stringify({
+            fileName: path.basename(filePath),
+        })
+
+        for (const row of rows) {
+            row.push(meta)
+        }
+    }
+
+    return new dfd.DataFrame(rows, {
+        columns,
     })
 }
 
 /**
  *
  * @param {string} fileList csv file path
+ * @param {CatOptions} options cat option
  * @returns {dfd.DataFrame} danfo DataFrame
  */
-const catUsingFileList = (fileList) => {
+const catUsingFileList = (fileList, options) => {
     const dfList = []
 
     for (const file of fileList) {
-        dfList.push(buildDataFrameFromCSVFilePath(file))
+        dfList.push(buildDataFrameFromCSVFilePath(file, options))
     }
     const combinedDataFrame = dfd.concat({ dfList, axis: 0 })
 
